bc041b072440c2b40af8184c2e49f28053641d8f
TranVi <vit76404@gmail.com>
2026-04-18 16:33:14 +0700

    fix frontend as normal folder

---
+++ frontend/src/components/auth/singin-form.tsx
@@ -0,0 +1,102 @@
+import { cn } from "@/lib/utils";
+import { Card, CardContent } from "@/components/ui/card";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+const signInSchema = z.object({
+  email: z.email("Email không hợp lệ"),
+  password: z.string().min(6, "Mật khẩu phải có ít nhất 6 ký tự"),
+});
+type SignInFormValues = z.infer<typeof signInSchema>;
+export function SigninForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignInFormValues>({
+    resolver: zodResolver(signInSchema),
+  });
+  const onSubmit = async (data: SignInFormValues) => {};
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0 border-border">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form onSubmit={handleSubmit(onSubmit)} className="p-6 md:p-8">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col items-center text-center gap-2">
+                <a href="/" className="mx-auto block w-fit text-center">
+                  <img src="/logo.svg" alt="logo" />
+                </a>
+                <h1 className="text-2xl font-bold">Chào mừng quay lại</h1>
+                <p className="text-muted-foreground text-balance">
+                  Đăng nhập vào tài khoản Moji của bạn
+                </p>
+              </div>
+              <div className="flex flex-col gap-3">
+                <Label htmlFor="email" className="block text-sm">
+                  Email
+                </Label>
+                <Input
+                  type="email"
+                  id="email"
+                  placeholder="gmail.com"
+                  {...register("email")}
+                />
+                {errors.email && (
+                  <p className="text-destructive text-sm">
+                    {errors.email.message}
+                  </p>
+                )}
+              </div>
+              <div className="flex flex-col gap-3">
+                <Label htmlFor="password" className="block text-sm">
+                  Mật Khẩu
+                </Label>
+                <Input
+                  type="password"
+                  id="password"
+                  {...register("password")}
+                />
+                {errors.password && (
+                  <p className="text-destructive text-sm">
+                    {errors.password.message}
+                  </p>
+                )}
+              </div>
+              <Button
+                type="submit"
+                className="w-full cursor-pointer"
+                disabled={isSubmitting}
+              >
+                Đăng nhập
+              </Button>
+              <div className="text-center text-sm cursor-pointer">
+                Chưa có tài khoản ?{" "}
+                <a href="/signup" className="underline underline-offset-4">
+                  Đăng ký
+                </a>
+              </div>
+            </div>
+          </form>
+          <div className="relative hidden bg-muted md:block">
+            <img
+              src="/placeholderSignUp.png"
+              alt="Image"
+              className="absolute top-1/2 -translate-y-1/2 object-cover"
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <div className="text-xs text-balance px-6 text-center *:[a]:hover:text-primary text-muted-foreground *:[a]:underline *:[a]:underline-offset-4">
+        Bằng cách tiếp tục,bạn đồng ý với <a href="#">Điều khoản dịch vụ </a> và{" "}
+        <a href="#">Chính sách bảo mật</a> của chúng tôi.
+      </div>
+    </div>
+  );
+}
